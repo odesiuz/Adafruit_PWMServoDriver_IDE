d46b40320e570d28a7f9a6227a132b37ca21e3fb
odesiuz <uviase_o@yahoo.com>
2018-01-20 12:21:09 +0000

    Update on AdafruitPWMServoDriver
    made read8 public to read data from servos

--- src/Adafruit_PWMServoDriver.h
+++ src/Adafruit_PWMServoDriver.h
@@ -57,11 +57,11 @@ class Adafruit_PWMServoDriver {
   void setPWMFreq(float freq);
   void setPWM(uint8_t num, uint16_t on, uint16_t off);
   void setPin(uint8_t num, uint16_t val, bool invert=false);
-
+  uint8_t read8(uint8_t addr);
+  
  private:
   uint8_t _i2caddr;
-
-  uint8_t read8(uint8_t addr);
+  
   void write8(uint8_t addr, uint8_t d);
 };
 
